c39ae8007b21e5588f0e81aa8075ec73b632b712
Tom Ouellette <t.ouellette@mail.utoronto.ca>
2024-09-25 03:01:39 -0400

    Removing old code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,22 +23,3 @@ fn main() {
         None => {}
     }
 }
-// use pineapple::index::JumpIndex;
-// use pineapple::utils::progress_log;
-// 
-// fn main() -> () {
-//     progress_log("Initializing JUMP download");
-// 
-//     let mut index = JumpIndex::init();
-//     
-//     let compound = Some("KYRVNWMVYQXFEU-UHFFFAOYSA-N".to_string());
-//     index.query(None, None, None, None, None, compound);
-// 
-//     let n_queries = index.queries.len();
-// 
-//     progress_log(format!("Detected {} samples for downloading.", n_queries).as_str());
-// 
-//     index.download(Some("test_images/".to_string()), Some(6));
-// 
-//     progress_log("Download complete.");
-// }
